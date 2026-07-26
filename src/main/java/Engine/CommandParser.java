de64ee83a35a2bd1cbe0aaa64d329037b0cbdada
vythili6 <yo.vythilingum@mail.utoronto.ca>
2026-07-26 19:48:29 -0400

    implementing the private helpers of Command Parser

--- src/main/java/Engine/CommandParser.java
+++ src/main/java/Engine/CommandParser.java
@@ -2,8 +2,7 @@ package Engine;
 
 import utilities.SynonymBuilder;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class CommandParser {
 
@@ -19,4 +18,27 @@ public class CommandParser {
     // normalizing, tokenizing (eseentially split the iput into list of words),
     // removal of stop words, resolve the verb by using the synonym map,
     // extract the target, assemble the command object.
+
+    private String normalization(String rawInput) {
+        String normalizedInput = rawInput.replaceAll("\\p{Punct}", " ");
+        return normalizedInput.trim().toLowerCase();
+    }
+
+    private List<String> tokenization(String sanitizedInput) {
+        String[] wordArray = sanitizedInput.split(" ");
+        return new ArrayList<>(Arrays.asList(wordArray));
+    }
+
+    private List<String> stopWordRemoval(List<String> wordList) {
+        List<String> stopWordFree = new ArrayList<>();
+        for (String word : wordList) {
+            if (!stopWordSet.contains(word)) {
+                stopWordFree.add(word);
+            }
+        }
+        return stopWordFree;
+    }
+
+
+
 }
